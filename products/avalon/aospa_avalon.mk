252c31b978aae39e3cea83e213381f37c732d778
anky894 <ko282ko82@gmail.com>
2026-01-03 11:24:24 +0530

    products: Add avalon
    Change-Id: I53d745a91259850069d85c99b6e301a4ff1975d4

---
+++ products/avalon/aospa_avalon.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2025 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq (aospa_avalon,$(TARGET_PRODUCT))
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from avalon device
+$(call inherit-product, device/oneplus/avalon/device.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
+
+PRODUCT_NAME := aospa_avalon
+PRODUCT_DEVICE := avalon
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2661
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+endif
